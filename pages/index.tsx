a320332f4bbc04a103ccabe8bb7c7dae539f0019
Rendi Nur Holis <holis112@gmail.com>
2025-04-23 15:22:32 +0700

    update project TinyLlama

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+// pages/index.tsx
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to TinyLlama Chatbot!</h1>
+    </div>
+  );
+};
+
+export default Home;
